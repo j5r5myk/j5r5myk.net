449e1cb5acba924722344f60d4f447ccf777cb91
Jeremy Kleiman <j5r5myk@gmail.com>
2018-03-27 18:14:04 -0400

    draw lines around viewport

--- scripts.js
+++ scripts.js
@@ -2,41 +2,40 @@ $(document).ready(function(){
 	// init page
 	var width = $(window).width() - 2*parseInt($('body').css('margin-left'));
 	var height = $(window).height() - 2*parseInt($('body').css('margin-top'));
-	width = .5* width;
-	height = .5 * height;
+	width = width;
+	height = height;
 	$("#dimensions").text("Your viewport is " + $(window).width() + " by " + $(window).height());
-	//setPositions(width, height);
+	setPositions(width, height);
 	draw(width, height);
 	// resizing
     $(window).resize(function() {
 		$("#dimensions").text("Your viewport is " + $(window).width() + " by " + $(window).height());
 		var width = $(window).width() - 2*parseInt($('body').css('margin-left'));
 		var height = $(window).height() - 2*parseInt($('body').css('margin-top'));
-		//setPositions(width, height);
+		setPositions(width, height);
 		draw(width, height);
 	});
-	
 });
 // draw a square around the viewport
 var draw = function(width, height) {
-	width = width+'px';
-	height = height+'px';
+	width = width;
+	height = height;
 	console.log(width, height);
 	// erase lines first
-	 $(".border").hide();
-    $("#top").show().animate({width: "318px"},1000, function() {
-      $("#right").show().animate({height: "238px"},1000, function() {
-        $("#bottom").show().animate({width: "318px"},1000, function() {
-          $("#left").show().animate({height: "238px"},1000, function() {
-          });
-        });
-      });
+	$(".border").hide();
+    $("#top").show().animate({width: width-2},1000, function() {
+		$('#right').show();
+		$('#right').animate({height: height-2},1000, function() {
+			$('#bottom').show();
+			$('#bottom').animate({width: width-2},1000, function() {
+				$('#left').show();
+				$('#left').animate({height: height-2},1000);
+			});
+		});
     });
 }
 // set positions of line segments
 var setPositions = function(width, height) {
-	$('#top').css({top: 0, left: 0});
-	$('#right').css({top: 0, left: width});
-	$('#bottom').css({top: height, left: width});
-	$('#left').css({top: height, left: 0});
+	$('#container').css({width: width, height: height});
+	$('#content').css({width: width-4, height: height-4});
 }
